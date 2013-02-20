43bba9ab706f1b867c472b1ff28afc84bb62ffea
skiener <skiener@skiener-PC>
2013-02-20 17:58:46 +0100

    "no funcionara la clase javaJesus porque es un proyecto web y no un proyecto java"

---
+++ PHP/web/javaJesus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author skiener
+ */
+public class javaJesus {
+    public static void main(String[] args){
+      System.out.print("Hola");
+    }
+}
